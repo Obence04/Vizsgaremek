5b4d93f4be8b4a4a861f9bb6bb933eebda5832ba
nemethur <atisnemeth73@gmail.com>
2024-03-22 13:28:49 +0100

    240322-4 push
    órarend szépítgetés

--- Naplo/resources/views/orarend.blade.php
+++ Naplo/resources/views/orarend.blade.php
@@ -1,18 +1,19 @@
 @extends('layout_fooldal')
 @section('content')
 <main class="container text-center">
-    <table>
+    <div class="table-responsive-xxl pt-5">
+    <table class="table mb-0" id="table">
         <thead>
            <tr>
-              <td id="sub1">         </td>
-              <td id="sub">8:30-9:30</td>
-              <td id="sub">9:30-10:30</td>
-              <td id="sub">10:30-11:30</td>
-              <td id="sub">11:30-12:30</td>
-              <td id="sub">12:30-2:00</td>
-              <td id="sub">2:00-3:00</td>
-              <td id="sub">3:00-4:00</td>
-              <td id="sub">4:00-5:00</td>
+              <td id="sub1"></td>
+              <td id="sub">8:00-8:45</td>
+              <td id="sub">9:00-9:45</td>
+              <td id="sub">10:00-10:45</td>
+              <td id="sub">11:00-11:45</td>
+              <td id="sub">12:00-12:30</td>
+              <td id="sub">12:30-13:15</td>
+              <td id="sub">13:30-14:15</td>
+              <td id="sub">14:30-15:15</td>
            </tr>
         </thead>
         <tbody>
@@ -22,7 +23,7 @@
               <td id="box1">SUB1</td>
               <td id="box2">SUB2</td>
               <td id="box3">SUB3</td>
-              <td rowspan="6">L<br>U<br>N<br>C<br>H
+              <td rowspan="6" id="lunch" class="text-center">Ebéd szünet</td>
               <td id="box4">SUB4</td>
               <td id="box5">SUB5</td>
               <td id="s">counselling class</td>
@@ -75,5 +76,6 @@
            </tr>
         </tbody>
      </table>
+    </div>
 </main>
 @endsection
